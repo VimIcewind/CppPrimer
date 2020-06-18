30a6c52a5f9bbefa83c9e3b674e746812b7f64c5
VimIcewind <VimIcewind@gmail.com>
2020-06-18 14:00:33 +0800

    array demo

---
+++ src/ch02/array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int ia[10];
+	int index;
+
+	for (index = 0; index < 10; ++index)
+		ia[index] = index;
+	for (index = 9; index >= 0; --index)
+		cout << ia[index] << " ";
+	cout << endl;
+
+	return 0;
+}
