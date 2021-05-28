9f1771b833572299f4069b4c6054a4a08397b402
VimIcewind <VimIcewind@gmail.com>
2021-05-28 14:58:44 +0800

    loop

---
+++ src/ch01/loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int iterations = 0;
+	bool continue_loop = true;
+
+	while (continue_loop != false) {
+		iterations++;
+		cout << "the while loop has executed "
+			<< iterations << " times\n";
+
+		if (iterations == 5)
+			continue_loop = false;
+	}
+}
