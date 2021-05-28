0a83d82a4b50c6cab03f52728f33febc84bf1538
VimIcewind <VimIcewind@gmail.com>
2021-05-28 15:24:15 +0800

    __cplusplus

---
+++ src/ch01/cplusplus.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#ifdef __cplusplus
+	// 不错，我们要编译 C++
+	// extern "C" 到第7章再解释
+	extern "C"
+#endif
+
+int min(int, int);
+
+int main(void)
+{
+
+	return 0;
+}
