ea8383b44add69c264c25cc97f48355ee963a989
VimIcewind <VimIcewind@gmail.com>
2021-06-01 11:01:18 +0800

    string as char array

---
+++ src/ch03/strlen.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+const char *st = "The expense of spirit\n";
+
+int main(void)
+{
+	int len = 0;
+	const char *p = st;
+
+	while (*p++)
+		++len;
+	cout << len << ": " << st;
+
+	return 0;
+}
