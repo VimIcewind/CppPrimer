592e6d5d3f0905d8ee3f20d23f2f6984bfaa14f8
VimIcewind <VimIcewind@gmail.com>
2021-06-01 10:31:46 +0800

    foobar

---
+++ src/ch03/foobar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int foobar(int *pi) {
+	if (pi) {
+		*pi = 1024;
+		return *pi;
+	} else {
+		return 0;
+	}
+}
+
+int main(void)
+{
+	int ival2 = 0;
+	int ival = foobar(&ival2);
+
+	return 0;
+}
