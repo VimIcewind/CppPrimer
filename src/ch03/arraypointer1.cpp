28381192b1bd3cda337dcda8bfc04b46a162903e
VimIcewind <VimIcewind@gmail.com>
2021-06-02 15:10:11 +0800

    ia_print

---
+++ src/ch03/arraypointer1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void ia_print(int *pbegin, int *pend)
+{
+	while (pbegin != pend) {
+		cout << *pbegin << ' ';
+		++pbegin;
+	}
+}
+
+int main(void)
+{
+	int ia[9] = {0, 1, 1, 2, 3, 5, 8, 18, 21};
+	ia_print(ia, ia + 9);
+
+	return 0;
+}
