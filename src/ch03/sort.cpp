097fc6b24d0ab8a61bb3cb57a5a468e8b31fac95
VimIcewind <VimIcewind@gmail.com>
2021-06-02 15:25:54 +0800

    sort

---
+++ src/ch03/sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int ia[6] = {107, 28, 3, 47, 104, 76};
+	string sa[3] = {"piglet", "eeyore", "pooh"};
+
+	sort(ia, ia+6);
+	sort(sa, sa+3);
+
+	for (int i = 0; i < 6; i++) {
+		cout << ia[i] << ' ';
+	}
+	cout << endl;
+	for (int i = 0; i < 3; i++) {
+		cout << sa[i] << ' ';
+	}
+
+	return 0;
+}
