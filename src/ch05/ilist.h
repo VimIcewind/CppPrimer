b3b11744085ff70eb35438957a561062abfcf471
VimIcewind <VimIcewind@gmail.com>
2021-06-12 22:23:40 +0800

    ilist

--- src/ch05/ilist.h
+++ src/ch05/ilist.h
@@ -1,7 +1,49 @@
 #ifndef ILIST_H
 #define ILIST_H
 
-class ilist_item;
+#include <iostream>
+
+using namespace std;
+
+class ilist_item {
+public:
+	ilist_item(int value, ilist_item *item_to_link_to = 0);
+
+	int value()
+	{
+		return _value;
+	}
+
+	ilist_item* next()
+	{
+		return _next;
+	}
+
+	void next(ilist_item *link)
+	{
+		_next = link;
+	}
+
+	void value(int new_value)
+	{
+		_value = new_value;
+	}
+
+private:
+	int _value;
+	ilist_item *_next;
+};
+
+inline ilist_item::ilist_item(int value, ilist_item *item) : _value(value)
+{
+	if (!item)
+		_next = 0;
+	else {
+		_next = item->_next;
+		// TODO 检查下面一条语句是否正确
+		item->_next = this;
+	}
+}
 
 class ilist {
 public:
@@ -9,9 +51,25 @@ public:
 
 	int size();
 
+	void insert_front(int value);
+	void insert_end(int value);
+	void insert(ilist_item *ptr, int value);
+
+	void display(ostream &os = cout);
+
+	ilist_item* find(int value);
+
+	void remove_front();
+	void remove_all();
+
+	int remove(int value);
+
 private:
 	ilist(const ilist&);
 	ilist& operator=(const ilist&);
+
+	void bump_up_size();
+	void bump_down_size();
 
 	ilist_item *_at_front;
 	ilist_item *_at_end;
@@ -22,6 +80,129 @@ private:
 inline int ilist::size()
 {
 	return _size;
+}
+
+inline void ilist::bump_up_size()
+{
+	++_size;
+}
+
+inline void ilist::bump_down_size()
+{
+	--_size;
+}
+
+inline void ilist::insert_front(int value)
+{
+	ilist_item *ptr = new ilist_item(value);
+	if (!_at_front)
+		_at_front = _at_end = ptr;
+	else {
+		ptr->next(_at_front);
+		_at_front = ptr;
+	}
+	bump_up_size();
+}
+
+inline void ilist::insert_end(int value)
+{
+	if (!_at_end)
+		_at_end = _at_front = new ilist_item(value);
+	else
+		_at_end = new ilist_item(value, _at_end);
+	bump_up_size();
+}
+
+inline void ilist::insert(ilist_item *ptr, int value)
+{
+	if (!ptr)
+		insert_front(value);
+	else {
+		bump_up_size();
+		new ilist_item(value, ptr);
+	}
+}
+
+ilist_item* ilist::find(int value)
+{
+	ilist_item *ptr = _at_front;
+
+	while (ptr)
+	{
+		if (ptr->value() == value)
+			break;
+		ptr = ptr->next();
+	}
+
+	return ptr;
+}
+
+void ilist::display(ostream &os)
+{
+	os << "\n( " << _size << " ) ( ";
+	ilist_item *ptr = _at_front;
+
+	while (ptr) {
+		os << ptr->value() << " ";
+		ptr = ptr->next();
+	}
+
+	os << ")\n";
+}
+
+inline void ilist::remove_front()
+{
+	if (_at_front) {
+		ilist_item *ptr = _at_front;
+		_at_front = _at_front->next();
+		bump_down_size();
+
+		delete ptr;
+	}
+}
+
+inline void ilist::remove_all()
+{
+	while (_at_front)
+		remove_front();
+
+	_size = 0;
+	_at_front = _at_end = 0;
+}
+
+int ilist::remove(int value)
+{
+	ilist_item *plist = _at_front;
+	int elem_cnt = 0;
+
+	while (plist && plist->value() == value) {
+		plist = plist->next();
+		remove_front();
+		++elem_cnt;
+	}
+
+	if (!plist)
+		return elem_cnt;
+	ilist_item *prev = plist;
+	plist = plist->next();
+
+	while (plist) {
+		if (plist->value() == value) {
+			prev->next(plist->next());
+			delete plist;
+			++elem_cnt;
+			bump_down_size();
+			plist = prev->next();
+			if (!plist) {
+				_at_end = prev;
+				return elem_cnt;
+			}
+		} else {
+			prev = plist;
+			plist = plist->next();
+		}
+	}
+	return elem_cnt;
 }
 
 #endif /* end of include guard: ILIST_H */
--- src/ch05/ilistTest.cpp
+++ src/ch05/ilistTest.cpp
@@ -6,8 +6,39 @@ using namespace std;
 int main()
 {
 	ilist mylist;
-	// ilist yourlist(mylist);
-	// mylist = yourlist;
+
+	for (int ix = 0; ix < 10; ++ix) {
+		mylist.insert_front(ix);
+		mylist.insert_end(ix);
+	}
+
+	cout << "OK: after insert_front() and insert_end()\n";
+	mylist.display();
+
+	ilist_item *it = mylist.find(8);
+
+	cout << "\n"
+		<< "Searching for the value 8: found it<<"
+		<< (it ? " yes!\n" : " no!\n" );
+
+	cout << "\n"
+		<< "Inserting element 1024 following the value 8\n";
+	mylist.display();
+
+	int elem_cnt = mylist.remove(8);
+
+	cout << "\n"
+		<< "Removed " << elem_cnt << " of the value 8\n";
+	mylist.display();
+
+	cout << "\n" << "Removed front element\n";
+
+	mylist.remove_front();
+	mylist.display();
+
+	cout << "\n" << "Removed all elements\n";
+
+	mylist.remove_all(); mylist.display();
 
 	return 0;
 }
