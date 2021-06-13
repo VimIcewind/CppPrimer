14c17ace3e837aca239e6eb6885c93307bf72822
VimIcewind <VimIcewind@gmail.com>
2021-06-13 14:44:15 +0800

    ilist() operator=

--- src/ch05/ilist.h
+++ src/ch05/ilist.h
@@ -49,11 +49,16 @@ class ilist {
 public:
 	ilist() : _at_front(0), _at_end(0), _size(0) {}
 
+	ilist(const ilist&);
+
+	ilist& operator=(const ilist&);
+
 	int size();
 
 	void insert_front(int value);
 	void insert_end(int value);
 	void insert(ilist_item *ptr, int value);
+	void insert_all(const ilist&);
 
 	void display(ostream &os = cout);
 
@@ -68,18 +73,43 @@ public:
 
 	void reverse();
 
-private:
-	ilist(const ilist&);
-	ilist& operator=(const ilist&);
+	ilist_item* init_iter(ilist_item *it = 0);
+	ilist_item* next_iter();
 
+private:
 	void bump_up_size();
 	void bump_down_size();
 
 	ilist_item *_at_front;
 	ilist_item *_at_end;
+	ilist_item *_current;
 
 	int _size;
 };
+
+inline void ilist::insert_all(const ilist &rhs)
+{
+	ilist_item *pt = rhs._at_front;
+
+	while (pt) {
+		insert_end(pt->value());
+		pt = pt->next();
+	}
+}
+
+inline ilist::ilist(const ilist &rhs) : _at_front(0), _at_end(0)
+{
+	insert_all(rhs);
+}
+
+inline ilist& ilist::operator=(const ilist &rhs)
+{
+	if (this != &rhs) {
+		remove_all();
+		insert_all(rhs);
+	}
+	return *this;
+}
 
 inline int ilist::size()
 {
@@ -159,8 +189,11 @@ inline void ilist::remove_front()
 	if (_at_front) {
 		ilist_item *ptr = _at_front;
 		_at_front = _at_front->next();
-		bump_down_size();
 
+		if (_current == ptr)
+			_current = _at_front;
+
+		bump_down_size();
 		delete ptr;
 	}
 }
@@ -193,6 +226,10 @@ int ilist::remove(int value)
 	while (plist) {
 		if (plist->value() == value) {
 			prev->next(plist->next());
+
+			if (_current == plist)
+				_current = prev->next();
+
 			delete plist;
 			++elem_cnt;
 			bump_down_size();
@@ -234,6 +271,18 @@ void ilist::reverse()
 		ptr = tmp;
 	}
 	_at_front->next(prev);
+}
+
+inline ilist_item* ilist::init_iter(ilist_item *it)
+{
+	return _current = it ? it : _at_front;
+}
+
+inline ilist_item* ilist::next_iter()
+{
+	ilist_item *next = _current ? _current = _current->next() : _current;
+
+	return next;
 }
 
 #endif /* end of include guard: ILIST_H */
