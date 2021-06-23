cc823859698434bae82e86ec1f99bff507515cb4
VimIcewind <VimIcewind@gmail.com>
2021-06-23 17:36:34 +0800

    assign

---
+++ src/ch06/assign.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	list<string> slist1(10);
+	list<string> slist2(24);
+
+	// slist 1 含有 10 个元素，slist 2 含有 24 个元素
+	cout << slist1.size() << ' ' << slist2.size() << endl;
+	slist1 = slist2;
+	// 赋值之后都有 24 个元素
+	cout << slist1.size() << ' ' << slist2.size() << endl;
+
+	return 0;
+}
