bd8e747bb5c84497916a53ad38ba1dcdab574585
VimIcewind <VimIcewind@gmail.com>
2021-06-24 15:22:56 +0800

    string::size_type

--- src/ch06/find.cpp
+++ src/ch06/find.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
 	string name("AnnaBelle");
-	int pos = name.find("Anna");
+	string::size_type pos = name.find("Anna");
 
 	if (pos == string::npos)
 		cout << "Anna not found!\n";
